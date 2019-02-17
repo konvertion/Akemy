05edaa50d6ae5be8ce49beb12c9ec1f6ede87df8
Juan Camilo Vargas <camilo.vargas.aguirre6444@gmail.com>
2019-02-16 21:01:36 -0500

    Caminata completa
    El personaje camina en ambos sentidos

--- Experiencia Visual/Assets/Escenarios/partícula/caminata.cs
+++ Experiencia Visual/Assets/Escenarios/partícula/caminata.cs
@@ -4,24 +4,43 @@ using UnityEngine;
 
 public class caminata : MonoBehaviour
 {
-    public float speed = 2;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    public float speed = 2f;
+
+    
+    
 
     // Update is called once per frame
     void Update()
     {
+       
        if(Input.GetKey(KeyCode.D))
         {
-            transform.Translate(new Vector2(speed, 0));
-        }
 
+            if(GetComponent<SpriteRenderer>().flipX)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            transform.Translate(new Vector2(speed, 0));
+            GetComponent<Animator>().SetBool("Correr", true);
+           
+        }
+       
         if (Input.GetKey(KeyCode.A))
         {
+            if (!GetComponent<SpriteRenderer>().flipX)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
             transform.Translate(new Vector2(-speed, 0));
+            GetComponent<Animator>().SetBool("Correr", true);
         }
+
+        if (Input.GetKeyUp(KeyCode.D) || (Input.GetKeyUp(KeyCode.A)))
+        {
+            GetComponent<Animator>().SetBool("Correr", false);
+        }
+
+
     }
 }
