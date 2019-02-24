02e4e83591e39b8d81cab5adba17b496978bd139
Juan Camilo Vargas <camilo.vargas.aguirre6444@gmail.com>
2019-02-23 22:36:34 -0500

    Cambios de Escenas listo
    (Eliminé el pre fab por que me daba problemas a la hora de hacer scripts)

---
+++ Proyecto Unity/Experiencia Visual/Assets/Scripts/PasaEscen2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PasaEscen2 : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+    void OnBecameInvisible()
+    {
+        SceneManager.LoadScene("Acto 2");
+    }
+}
