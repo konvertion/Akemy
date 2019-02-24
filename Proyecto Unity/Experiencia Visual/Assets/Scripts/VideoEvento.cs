83b67586108176ecd025cb80b8e790a0283e882d
Juan Camilo Vargas <camilo.vargas.aguirre6444@gmail.com>
2019-02-24 16:41:42 -0500

    Musica sin cortarse

--- Proyecto Unity/Experiencia Visual/Assets/Scripts/VideoEvento.cs
+++ Proyecto Unity/Experiencia Visual/Assets/Scripts/VideoEvento.cs
@@ -15,7 +15,7 @@ public class VideoEvento : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        musicPlayer = Camera.main.gameObject.GetComponent<AudioSource>();
+        musicPlayer = gameObject.GetComponent<AudioSource>();
     }
 
 
